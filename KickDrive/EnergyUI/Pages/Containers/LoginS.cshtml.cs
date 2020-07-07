1be6333ae9a1d9ef4c5f46c1962dc853f11e8593
Ravell <joelaaron19@outlook.es>
2020-07-06 21:42:24 -0500

    Diseño del login

---
+++ KickDrive/EnergyUI/Pages/Containers/LoginS.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EnergyUI.Pages.Containers
+{
+    public class LoginSModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
